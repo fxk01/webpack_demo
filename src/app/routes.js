9dc6e6f8fb068aa8ce8d7850da544e0632c1846d
fxk01 <2416113928@qq.com>
2018-09-04 14:32:07 +0800

    路由配置

--- src/app/routes.js
+++ src/app/routes.js
@@ -2,17 +2,30 @@ let routes = [
   {
     name: 'home',
     path: '/home',
-    url: './src/pages/home.html',
+    async(routeTo, routeFrom, resole, reject) {
+      if (routeTo.name === 'home') {
+        resole({ url: './src/pages/home.html' })
+      }
+    },
   },
   {
     name: 'about',
     path: '/about',
-    url: './src/pages/about.html',
+    async(routeTo, routeFrom, resole, reject) {
+      if (routeTo.name === 'about') {
+        resole({ url: './src/pages/about.html' })
+      }
+    },
   },
   {
     name: 'list',
     path: '/list',
     url: './src/pages/list.html',
+    async(routeTo, routeFrom, resole, reject) {
+      if (routeTo.name === 'list') {
+        resole({ url: './src/pages/list.html' })
+      }
+    },
   },
   {
     path: '(.*)',
